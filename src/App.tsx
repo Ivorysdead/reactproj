7113e610613dd89fa936199007a2cf1ba890b0da
Quinn2328 <113200011+Ivorysdead@users.noreply.github.com>
2024-10-15 18:40:45 -0500

    Rendering lists

--- src/App.tsx
+++ src/App.tsx
@@ -3,35 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 
-const user = {
-    name: 'Hedy Lamarr',
-    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
-    imageSize: 90,
-};
+const products = [
+    { title: 'Cabbage', isFruit: false, id: 1 },
+    { title: 'Garlic', isFruit: false, id: 2 },
+    { title: 'Apple', isFruit: true, id: 3 },
+];
 
-function AdminPanel() {
-    return null;
-}
+export default function ShoppingList() {
+    const listItems = products.map(product =>
+        <li
+            key={product.id}
+            style={{
+                color: product.isFruit ? 'magenta' : 'darkgreen'
+            }}
+        >
+            {product.title}
+        </li>
+    );
 
-function LoginForm() {
-    return null;
-}
-
-export default function Profile() {
-    let content;
     return (
-        <>
-            if (isLoggedIn) {
-            content = <AdminPanel/>
-        } else {
-            content = <LoginForm/>
-        }
-            return (
-            <div>
-                {content}
-            </div>
-            )
-        </>
+        <ul>{listItems}</ul>
     );
 }
-
