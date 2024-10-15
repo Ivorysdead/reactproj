40a0ee6e613a0e8cc7e491e04c9b4614cfa616f8
Quinn2328 <113200011+Ivorysdead@users.noreply.github.com>
2024-10-15 14:35:32 -0500

    Writing markup with JSX

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function MyButton() {
     return (
         <button>
@@ -10,11 +11,20 @@ function MyButton() {
     );
 }
 
-export default function MyApp() {
+function MyApp() {
     return (
         <div>
             <h1>Welcome to my app</h1>
             <MyButton />
+
         </div>
+    );
+}
+export default function AboutPage() {
+    return (
+        <>
+            <h1>About</h1>
+            <p>Hello there.<br />How do you do?</p>
+        </>
     );
 }
