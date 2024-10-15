6d57414f3a9acf8fefcd0c6fe00fb01bf543d416
Quinn2328 <113200011+Ivorysdead@users.noreply.github.com>
2024-10-15 14:41:27 -0500

    Adding styles

--- src/App.tsx
+++ src/App.tsx
@@ -11,20 +11,22 @@ function MyButton() {
     );
 }
 
-function MyApp() {
+export default function MyApp() {
     return (
         <div>
             <h1>Welcome to my app</h1>
-            <MyButton />
+            <MyButton/>
 
         </div>
     );
 }
-export default function AboutPage() {
+
+function AboutPage() {
     return (
         <>
             <h1>About </h1>
-            <p>Hello there.<br />How do you do?</p>
+            <p>Hello there.<br/>How do you do?</p>
         </>
     );
 }
+<img className="avatar" alt=" React logo"/>
